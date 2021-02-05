5996d9713c9c949faf3c7a4b29ca11e40f0726cd
Dayenne C Souza <dayenne@exxonmobil.com>
2021-02-05 20:05:08 -0300

    first tests

---
+++ src/components/Congrats/Congrats.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Congrats({success}) {
+  return (
+    <>
+      <div data-test="component-congrats">
+        {success && (
+            <span data-test="congrats-message">
+              Congratulations! You guessed the word!
+            </span>
+        )}
+      </div>
+
+    </>
+  );
+}
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired,
+};
+
+export default Congrats;
