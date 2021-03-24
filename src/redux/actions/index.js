c345e127cc13b15c47a960b444b705c3ca8a0a57
Dayenne C Souza <dayenne@exxonmobil.com>
2021-03-24 17:47:15 -0300

    stop error

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,10 @@
 import { getLetterMatchCount} from '../../helpers';
+import axios from 'axios';
+
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD',
 }
 
 // export function correctGuess() {
@@ -11,7 +14,7 @@ export const actionTypes = {
 export const guessWord = (guessedWord) => {
   return function(dispatch, getState) {
     const secretWord = getState().secretWord;
-    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord)/
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
     
     dispatch({type: actionTypes.GUESS_WORD, payload: {guessedWord, letterMatchCount}});
     if(guessedWord === secretWord) {
@@ -19,3 +22,9 @@ export const guessWord = (guessedWord) => {
     }
   };
 };
+
+export const getSecretWord = () => {
+  return (dispatch) => {
+    axios.get('http://localhost:3030');
+  }
+}
